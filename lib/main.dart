56d611d741b49897aba736f88cab05ba4d00e316
Vitor Alves <vitor.asilva52@senacsp.edu.br>
2026-09-15 01:45:38 -0300

    Criando projeto de calculadora de IMC e das quatro operações básicas da matemática.

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:calculadoras/paginas/tela_principal.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Projeto UC12',
+      //Remove a faixa debug
+      debugShowCheckedModeBanner: false,
+      //dfefine o tema do aplicativo
+      theme: ThemeData(
+        //cor principal do app
+        primarySwatch: Colors.indigo,
+        //ativa o padrão visual mais recente do Material Design
+        useMaterial3: true,
+      ),
+      //define qual seria a primeira tela a ser exibida.
+      home: TelaPrincipal(),
+    );
+  }
+}
---
+++ lib/paginas/tela_principal.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class TelaPrincipal extends StatelessWidget {
+  const TelaPrincipal({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Calculadoras.'),
+        backgroundColor: const Color.fromARGB(255, 201, 16, 77),
+        foregroundColor: Colors.white,
+      ),
+
+
+      body: Padding(
+        //espaçamento em todas as áreas da aplicação
+        padding: const EdgeInsets.all(24),
+
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+
+            children: [
+
+
+              const Icon(
+                Icons.calculate,
+                size: 100,
+                color: Colors.lightGreen
+              ),
+
+              const Text(
+                'Calculadora IMC',
+                style: TextStyle(
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                ),
+              )
+            ],
+        )
+      )
+    );
+  }
+}
